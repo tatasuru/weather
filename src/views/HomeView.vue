3130590143409bf55611a3635cf9354386badc74
小池　建 <t.koike@roseaupensant.jp>
2022-08-14 14:58:42 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      isDisplay: true
+    }
+  },
+  methods: {
+  }
+}
+</script>
